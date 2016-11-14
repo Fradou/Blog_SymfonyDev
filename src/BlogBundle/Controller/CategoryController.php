1736db88030c311dad222613868d6ad05aec72a8
Alexandre FRADET <a_fradet@yahoo.fr>
2016-11-14 15:39:52 +0100

    Create Article, Content & Category entity. Add EasyAdminBundle

---
+++ src/BlogBundle/Controller/CategoryController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace BlogBundle\Controller;
+
+use BlogBundle\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+/**
+ * Category controller.
+ *
+ */
+class CategoryController extends Controller
+{
+    /**
+     * Lists all category entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $categories = $em->getRepository('BlogBundle:Category')->findAll();
+
+        return $this->render('category/index.html.twig', array(
+            'categories' => $categories,
+        ));
+    }
+
+    /**
+     * Finds and displays a category entity.
+     *
+     */
+    public function showAction(Category $category)
+    {
+
+        return $this->render('category/show.html.twig', array(
+            'category' => $category,
+        ));
+    }
+}
---
+++ src/BlogBundle/Entity/Content.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Content
+ */
+class Content
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $category;
+
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @var string
+     */
+    private $other;
+
+    /**
+     * @var string
+     */
+    private $img;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Content
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set category
+     *
+     * @param string $category
+     * @return Content
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return string 
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set other
+     *
+     * @param string $other
+     * @return Content
+     */
+    public function setOther($other)
+    {
+        $this->other = $other;
+
+        return $this;
+    }
+
+    /**
+     * Get other
+     *
+     * @return string 
+     */
+    public function getOther()
+    {
+        return $this->other;
+    }
+
+    /**
+     * Set img
+     *
+     * @param string $img
+     * @return Content
+     */
+    public function setImg($img)
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    /**
+     * Get img
+     *
+     * @return string 
+     */
+    public function getImg()
+    {
+        return $this->img;
+    }
+}
