2a55d14e4083b3b7b87666a962441005fb42381c
Alexandre FRADET <a_fradet@yahoo.fr>
2016-11-19 19:26:02 +0100

    Finish the Contact pannel for frontpage. Create a small jq script to display mail on click. Change some routing and anchor. Reorder javascript to have no issue on loading (had jq loading last, not cool). Add some styling.

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,9 @@
 /**
  * Created by alex on 08/11/16.
  */
+$(document).ready(function () {
+    $("#maillink").click(function(){
+        $("#maillink").hide();
+        $("#mailelem").show();
+    });
+});
