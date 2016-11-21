cfe4b7a91a80d2224eab1a3650b3a33489b6d757
Alexandre FRADET <a_fradet@yahoo.fr>
2016-11-21 01:39:34 +0100

    Navbar is nearly finished. Styling looks ok, gonna check again route, path, disable. Start working on project page. Made a nice visual for technic. Add a fixed button

--- web/js/main.js
+++ web/js/main.js
@@ -2,12 +2,21 @@
  * Created by alex on 08/11/16.
  */
 $(document).ready(function () {
+    /** Display mail on frontpage **/
     $("#maillink").click(function(){
         $("#maillink").hide();
         $("#mailelem").show();
     });
+
+    /** In progress : open first log when clikcing on log title **/
     $("#log_title").click(function(){
 
         $(".log_article:first-child").simulate('mousedown');
     });
+
+    /** Materialize mobile menu **/
+    $(".button-collapse").sideNav({
+            menuWidth: 100
+        }
+    );
 });
