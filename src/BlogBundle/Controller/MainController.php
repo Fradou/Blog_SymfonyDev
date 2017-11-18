ea882514bd3c20dcd862bd50e87848b8af6b9392
Alexandre Fradet <alfradev@gmail.com>
2017-11-18 21:20:26 +0100

    Clean up remaining file / methods / routing linked to logbook. Add missing twig block for resume

--- src/BlogBundle/Controller/MainController.php
+++ src/BlogBundle/Controller/MainController.php
@@ -16,25 +16,6 @@ class MainController extends Controller
         ));
     }
 
-    public function logbookAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $logcoms = $em->getRepository('BlogBundle:Logcom')->findBy(
-            array(),
-            array('id' => 'desc'),
-            5,
-            0
-        );
-
-        $logbooks = $em->getRepository('BlogBundle:Logbook')->findBy(array(),array('date' => 'desc'));
-
-        return $this->render('Main/index.html.twig', array(
-            'logbooks' => $logbooks,
-            'logcoms' => $logcoms,
-        ));
-    }
-
     public function projectAction()
     {
         return $this->render('Main/project.html.twig', array(
